35433f5b0c098555054100de09d2513058856881
Rishi Vishwakarma <rishivishwakarma24x7@gmail.com>
2017-05-15 10:34:46 +0530

    Update init.js

--- scripts/init.js
+++ scripts/init.js
@@ -1,2 +1,4 @@
-  // Initialize Firebase
-  // TODO: Replace with your project's customized code snippet
+// Initialize Firebase
+// TODO: Replace with your project's customized code snippet
+if (typeof firebase === 'undefined') throw new Error('hosting/init-error: Firebase SDK not detected. You must include it before /__/firebase/init.js');
+firebase.initializeApp({});

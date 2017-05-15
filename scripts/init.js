e8075926564e2347de00bea66d802e287345bb6b
Rishi Vishwakarma <rishivishwakarma24x7@gmail.com>
2017-05-15 10:36:34 +0530

    Update init.js

--- scripts/init.js
+++ scripts/init.js
@@ -2,5 +2,10 @@
 // TODO: Replace with your project's customized code snippet
 if (typeof firebase === 'undefined') throw new Error('hosting/init-error: Firebase SDK not detected. You must include it before /__/firebase/init.js');
 firebase.initializeApp({
-
+  "apiKey": "<API_KEY>",
+  "databaseURL": "https://<PROJECT_ID>.firebaseio.com",
+  "storageBucket": "<PROJECT_ID>.appspot.com",
+  "authDomain": "<PROJECT_ID>.firebaseapp.com",
+  "messagingSenderId": "<SENDER_ID>",
+  "projectId": "<PROJECT_ID>"
 });
